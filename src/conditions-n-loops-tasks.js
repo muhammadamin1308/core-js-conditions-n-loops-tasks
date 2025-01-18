4e6126ad11daa20387f65619e76274e8ce90d867
Muhammadamin <79371764+muhammadamin1308@users.noreply.github.com>
2025-01-18 22:55:24 +0500

    fix: linter error fixed

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -367,8 +367,8 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(arr) {
-
+function sortByAsc(/* arr */) {
+  throw new Error('Not implemented');
 }
 
 /**
